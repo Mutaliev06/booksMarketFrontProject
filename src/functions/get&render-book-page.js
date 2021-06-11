9e08b744364cebe8271243d2bcadee25fa38bf00
azhigov <adam.azhigov97@gmail.com>
2021-06-11 20:17:55 +0300

    '123'

--- src/functions/get&render-book-page.js
+++ src/functions/get&render-book-page.js
@@ -38,18 +38,60 @@ module.exports = (item) => {
     const reviewInput = document.createElement('div')
     const nameInput = document.createElement('input')
     const textInput = document.createElement('input')
-    const btnInput = document.createElement('button')
+    const btnInput = document.createElement('input')
     const createInput = document.createElement('div')
-    createInput.append(nameInput, textInput, btnInput)
-    btnInput.append('Добавить')
+    const form = document.createElement('form')
+
+    form.append(nameInput, textInput, btnInput)
     reviewInput.append('Оставить отзыв:')
     reviewInput.append(createInput)
+    reviewInput.append()
+    createInput.append(form)
+
+
     review.append(reviewInput, 'Отзывы читателей:')
+    reviewInput.className = 'reviewInput'
+    createInput.className = 'createInput'
     nameInput.placeholder = 'Введите имя'
     textInput.placeholder = 'Введите отзыв'
+    form.method = 'post'
+    form.action = `#`
     btnInput.type = 'submit'
+    btnInput.value = 'добавить'
     nameInput.name = 'userName'
     textInput.name = 'text'
+    btnInput.addEventListener('click', (e) => {
+      e.preventDefault()
+      fetch(`http://localhost:3006/book/${item._id}/reviews`,{
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json', // отправляемые данные
+        },
+        body: JSON.stringify({
+          userName: nameInput.value,
+          text: textInput.value
+        })
+      })
+      .then(res => res.json())
+      .then(item => {
+        const reviewDiv = document.createElement('div')
+        const reviewUser = document.createElement('p')
+        const reviewText = document.createElement('p')
+        reviewDiv.className = 'reviewDiv'
+        reviewText.append(item.text)
+        reviewUser.append(item.userName)
+        reviewDiv.append(reviewUser, reviewText)
+
+        review.append(reviewDiv)
+
+        nameInput.value = ''
+        textInput.value = ''
+
+      })
+      // методом пост отправить данные на сервер (фетч)
+      //после получения новых данных создаем новый див и вставляем туда комментарий
+    })
+
     console.log(item._id)
 
 
